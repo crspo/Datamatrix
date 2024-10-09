02ccae2ab676e3d09673002980d3c4500bc03d5e
Chandan Pokhrel <crspokhrel@gmail.com>
2024-10-09 15:09:26 +1100

    Used GUI to request input text file

--- Datamatrix.py
+++ Datamatrix.py
@@ -2,20 +2,11 @@ from pylibdmtx.pylibdmtx import encode
 import os
 import re
 from PIL import Image, ImageDraw, ImageFont
+import tkinter as tk
+from tkinter import filedialog,messagebox,commondialog
+
 CURRENT_DIR = os.path.dirname(__file__)
-
-def read_serials_from_file(file):
-    """Read serial numbers from a file and return them as a list.
-
-    Args:
-        file (str): The path to the file containing the serial numbers.
-
-    Returns:
-        list: A list of strings representing the serial numbers.
-    """
-    with open(file, 'r') as file:
-        serials = [re.sub(r"\s+", "", line.strip()) for line in file.readlines()]
-    return serials
+global input_file
 
 def generate_datamatrix(serials, output_path):
     """
@@ -47,7 +38,7 @@ def generate_datamatrix(serials, output_path):
         # Combine the two images into one
         combined_width = img1.width + img2.width + 40
         combined_height = max(img1.height, img2.height) + 40
-        combined_img = Image.new('RGB', (combined_width, combined_height), "white")
+        img = Image.new('RGB', (combined_width, combined_height), "white")
 
         #creating drawing context for captions
         draw = ImageDraw.Draw(combined_img)
@@ -55,31 +46,37 @@ def generate_datamatrix(serials, output_path):
         draw.text((20, 5), 'QR for first 100 serials', fill="black", font=font)
         draw.text((img1.width + 40, 5), 'QR for remaining serials', fill="black", font=font)
         # Paste the images into the new image
-        combined_img.paste(img1, (10, 20))
-        combined_img.paste(img2, (img1.width + 20, 20))
-        
-
-        
-        combined_img.save(output_path)
-    
+        img.paste(img1, (10, 20))
+        img.paste(img2, (img1.width + 20, 20)) 
+        img.save(output_path)
     else:
         img = create_qr_image(serials)
         img.save(output_path)
+        
+    #messagebox.showinfo("SQR code generated successfully!", img.show())
+    img.show()
     
     
-def main(input_file, output_path):
+def main(output_path):
     """
     Generate a data matrix from a list of serial numbers.
     Parameters:
         input_file (str): The path to the file containing the serial numbers.
         output_path (str): The path to save the generated data matrices.
     """
-    serials = read_serials_from_file(input_file)
-    generate_datamatrix(serials, output_path)
-    print("Generated DataMatrix for all serials")
+    input_file = filedialog.askopenfilename(title="Select a Text File for serials", filetypes=[("Text files", "*.txt")])
+    
+    if input_file:
+        with open(input_file, 'r') as file:
+            serials = [re.sub(r"\s+", "", line.strip()) for line in file.readlines()]
+        generate_datamatrix(serials, output_path)
+        
+        
+        
+    else:
+        print("No file selected")
 
 if __name__ == "__main__":
-    input_file = os.path.join(CURRENT_DIR, "Data//serials.txt") 
-    output_path = os.path.join(CURRENT_DIR,"Data//Qrcode_for_all_serials.png")
-
-    main(input_file, output_path)
+    main(output_path = os.path.join(CURRENT_DIR,"Input serials and output image//Qrcode_for_all_serials.png"))
+    #input_file = os.path.join(CURRENT_DIR, "Input serials and output image//serials.txt") 
+    #output_path = os.path.join(CURRENT_DIR,"Input serials and output image//Qrcode_for_all_serials.png")
